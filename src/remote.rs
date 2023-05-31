9ec58cd81c3ce2c45ec6f350fc282b1ae0e9e571
Adam CAVILLON <contact@codinbox.fr>
2023-05-31 11:46:38 +0200

    Remove test function

--- src/remote.rs
+++ src/remote.rs
@@ -8,28 +8,6 @@ use crate::FILE_SPLITTER;
 use crate::local::tfs::BACKUP_GROUP_DIR;
 use crate::types::ctx;
 
-pub async fn test() {
-    info!("Creating credentials");
-    let creds = Credentials::new(Some(env::var("S3_KEY_ID").unwrap().as_str()), Some(env::var("S3_SECRET_KEY").unwrap().as_str()), None, None, None).unwrap();
-    info!("Credentials created");
-    let bucket = Bucket::new(env::var("S3_BUCKET_NAME").unwrap().as_str(), Region::Custom {
-        region: env::var("S3_REGION").unwrap().as_str().to_owned(),
-        endpoint: env::var("S3_ENDPOINT").unwrap().as_str().to_owned()
-    }, creds).unwrap();
-    // List all objects in a bucket
-    let result_future = bucket.list(String::from(""), None);
-    info!("Awaiting result");
-    let result = result_future.await;
-    match result {
-        Ok(listed) => {
-            info!("Got a list of objects!");
-        },
-        Err(e) => {
-            error!("Got an error listing bucket: {}", e);
-        }
-    }
-}
-
 fn get_prefix() -> String {
     let prefix = env::var("S3_PREFIX").unwrap();
     if prefix == "" {
